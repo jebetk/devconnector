654a7ff2a3f0576adf7faf374092a3e23085e5f3
Betty Kemboi <betty.j.kemboi@gmail.com>
2020-10-14 04:03:32 -0500

    Profile Reducer & Get Current Profile

---
+++ client/src/actions/profile.js
@@ -0,0 +1,18 @@
+axiosimport { axios } from 'axios';
+import { setAlert } from './alert';
+import { GET_PROFILE, PROFILE_ERROR } from './types';
+
+// Get current user's profile 
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get(/api/profile/me);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    }
+    )
+  } catch (err) {;
+    
+  }
+};
